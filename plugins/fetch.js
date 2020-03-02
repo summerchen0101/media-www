2ac7268cdde891a68e248f8bd5d2a303d33ef932
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-02 16:24:16 +0800

    修改Referer參數

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -38,7 +38,7 @@ export default ({ app, store, $axios, redirect, error }, inject) => {
       data,
       headers: {
         Authorization: `Bearer ${store.state.user.token}`,
-        Referer: store.state.log.host
+        Referer: `${process.env.PROTOCOL}://${store.state.log.host}`
       },
       ...config
     })
