09f469ebaaa7460439e95ef6f856e50f57583e7e
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-02 10:02:40 +0800

    加入error頁面

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">
+      404 Page not found
+    </h1>
+    <h1 v-else-if="error.statusCode === 403">
+      403 Permission denied
+    </h1>
+    <h1 v-else>
+      An error occurred
+    </h1>
+    <!-- <h3>{{ error.message }}</h3> -->
+    <nuxt-link to="/">
+      Home page
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
