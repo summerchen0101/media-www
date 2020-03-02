484aad9fe7787efd320c41bf8ce7562a799c9b95
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-02 11:33:50 +0800

    修改site api模組的namespaced

--- store/site/actions.js
+++ store/site/actions.js
@@ -1,12 +1,12 @@
 export default {
   async getInfo ({ commit }) {
-    const res = await this.$api.SITE.getInfo()
+    const res = await this.$api.getSiteInfo()
     if (res.code === '0') {
       commit('gotInfo', res.data)
     }
   },
   async getFaq ({ commit }) {
-    const res = await this.$api.SITE.getFaq()
+    const res = await this.$api.getFaq()
     if (res.code === '0') {
       commit('gotFaq', res.data)
     }
