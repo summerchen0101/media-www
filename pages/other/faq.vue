4083f0e94ae80122712aa3b6999bb7b1eba929ab
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-04 16:36:18 +0800

    faq編碼修正

--- pages/other/faq.vue
+++ pages/other/faq.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="company_right col-xs-12 col-sm-9 col-md-9">
     <div class="company_title">
-      <i class="icon-discuss-issue" /><span>常觝问题</span>
+      <i class="icon-discuss-issue" /><span>常见问题</span>
     </div>
     <div class="company_content">
       <div class="qa_container">
