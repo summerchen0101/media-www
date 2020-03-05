c30e5c7001946117d375d1e766a1474b4101a542
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-05 14:29:45 +0800

    排行榜頁的tab改動態

--- pages/tv/rank.vue
+++ pages/tv/rank.vue
@@ -10,90 +10,37 @@
         <div class="container">
           <!-- Nav tabs -->
           <ul class="nav nav-tabs ranking-tabs" role="tablist">
-            <li role="presentation " class="active">
-              <a href="#ranking2" aria-controls="ranking2" role="tab" data-toggle="tab">戏剧排行</a>
-            </li>
-            <li role="presentation">
-              <a href="#ranking3" aria-controls="ranking3" role="tab" data-toggle="tab">电影排行</a>
-            </li>
-            <li role="presentation">
-              <a href="#ranking4" aria-controls="ranking4" role="tab" data-toggle="tab">动漫排行</a>
-            </li>
-            <li role="presentation">
-              <a href="#ranking5" aria-controls="ranking5" role="tab" data-toggle="tab">综艺排行</a>
+            <li
+              v-for="(cate, index) in cates"
+              :key="index"
+              role="presentation"
+              :class="{active: currentTab === index}"
+            >
+              <a
+                :href="`#ranking${index}`"
+                :aria-controls="`#ranking${index}`"
+                role="tab"
+                data-toggle="tab"
+                @click="currentTab = index"
+              >{{ cate.label }}排行</a>
             </li>
           </ul>
           <!-- Tab panes -->
           <div class="tab-content ranking-tab-content">
-            <div id="ranking2" role="tabpanel" class="tab-pane active">
+            <div
+              v-for="(cate, index) in cates"
+              :id="`#ranking${index}`"
+              :key="index"
+              role="tabpanel"
+              class="tab-pane"
+              :class="{active: currentTab === index}"
+            >
               <div class="ranking-head">
-                <i class="icon-television" />戏剧排行榜 - 今日TOP100
+                <i :class="cate.icon" />{{ cate.label }}排行榜 - 今日TOP20
               </div>
               <div class="ranking-body">
                 <div class="tv_sub_list">
-                  <a v-for="i in 20" :key="i" href="tv_korea_detail.html" class="video_unit">
-                    <div class="tv_unit_img tv_sub_img imgLiquidFill imgLiquid">
-                      <img src="/images/tv_korea_list/tv_korea_list_img.jpg" alt="">
-                    </div>
-                    <div class="index_info">
-                      <div class="index_unit_title"> 明天和你</div>
-                      <div class="index_unit_txt">更新至6集</div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div id="ranking3" role="tabpanel" class="tab-pane">
-              <div class="ranking-head">
-                <i class="icon-movie-symbol-of-video-camera" />电影排行榜 - 今日TOP100
-              </div>
-              <div class="ranking-body">
-                <div class="tv_sub_list">
-                  <a v-for="i in 20" :key="i" href="tv_korea_detail.html" class="video_unit">
-                    <div class="tv_unit_img tv_sub_img imgLiquidFill imgLiquid">
-                      <img src="/images/tv_korea_list/tv_korea_list_img4.jpg" alt="">
-                    </div>
-                    <div class="index_info">
-                      <div class="index_unit_title"> 明天和你</div>
-                      <div class="index_unit_txt">更新至6集</div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div id="ranking4" role="tabpanel" class="tab-pane">
-              <div class="ranking-head">
-                <i class="icon-cartoons-character-with-big-eyes" />动漫排行榜 - 今日TOP100
-              </div>
-              <div class="ranking-body">
-                <div class="tv_sub_list">
-                  <a v-for="i in 20" :key="i" href="tv_korea_detail.html" class="video_unit">
-                    <div class="tv_unit_img tv_sub_img imgLiquidFill imgLiquid">
-                      <img src="/images/tv_korea_list/tv_korea_list_img3.jpg" alt="">
-                    </div>
-                    <div class="index_info">
-                      <div class="index_unit_title"> 明天和你</div>
-                      <div class="index_unit_txt">更新至6集</div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div id="ranking5" role="tabpanel" class="tab-pane">
-              <div class="ranking-head">
-                <i class="icon-karaoke-microphone-icon" />综艺排行榜 - 今日TOP100
-              </div>
-              <div class="ranking-body">
-                <div class="tv_sub_list">
-                  <a v-for="i in 20" :key="i" href="tv_korea_detail.html" class="video_unit">
-                    <div class="tv_unit_img tv_sub_img imgLiquidFill imgLiquid">
-                      <img src="/images/tv_korea_list/tv_korea_list_img2.jpg" alt="">
-                    </div>
-                    <div class="index_info">
-                      <div class="index_unit_title"> 明天和你</div>
-                      <div class="index_unit_txt">更新至6集</div>
-                    </div>
-                  </a>
+                  <VideoItem v-for="(item, i) in 20" :key="i" />
                 </div>
               </div>
             </div>
@@ -112,8 +59,41 @@ export default {
   name: 'Rank',
   layout: 'main',
   components: {},
+  asyncData ({ store, redirect, params }) {
+    return {
+      cates: [
+        {
+          label: '戏剧',
+          icon: 'icon-television'
+        },
+        {
+          label: '电影',
+          icon: 'icon-movie-symbol-of-video-camera'
+        },
+        {
+          label: '动漫',
+          icon: 'icon-cartoons-character-with-big-eyes'
+        },
+        {
+          label: '综艺',
+          icon: 'icon-karaoke-microphone-icon'
+        }
+      ]
+    }
+  },
   data () {
-    return {}
+    return {
+      currentTab: 0
+    }
+  },
+  watchQuery: true,
+  key: to => to.fullPath,
+  watch: {
+    currentTab () {
+      this.$nextTick(() => {
+        $('.imgLiquidFill').imgLiquid()
+      })
+    }
   },
   mounted () {
     $('.imgLiquidFill').imgLiquid()
