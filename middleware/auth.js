0f19d43f128722f516b8758ac4fe87474f40c60b
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-11 10:55:45 +0800

    修改middleware: auth.js 1. 無權限倒回首頁時跳「請重新登入」 2. 進入會員頁layout先撈profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,10 @@ export default function ({ store, redirect, req }) {
   if (process.server) {
     store.state.user.token = req.session.token
   }
-  if (!store.state.user.token) {
+  if (store.state.user.token) {
+    store.dispatch('user/getProfile')
+  } else {
     redirect('/')
+    this.$alert('請重新登入')
   }
 }
