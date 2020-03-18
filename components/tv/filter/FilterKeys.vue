47c013d000b0fa6759ff4d651049c7d6c082e896
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-18 12:24:29 +0800

    filter頁 篩選條件的結構樣式微調

--- components/tv/filter/FilterKeys.vue
+++ components/tv/filter/FilterKeys.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="filter_box">
-    <span class="filter_title">筛选条件：</span>
-    <a v-for="(item, i) in filterItems" :key="i" href="" @click.prevent="handleRemoveItem(item.type)">
-      {{ item.label }}<i class="fa fa-times-circle" />
-    </a>
-    <!-- <a href="">韩国<i class="fa fa-times-circle" /></a>
-    <a href="">2016<i class="fa fa-times-circle" /></a> -->
+    <div class="filter_result">
+      <span class="filter_title">筛选条件：</span>
+      <a v-for="(item, i) in filterItems" :key="i" href="" @click.prevent="handleRemoveItem(item.type)">
+        {{ item.label }}<i class="fa fa-times-circle" />
+      </a>
+    </div>
     <span class="result">共884个筛选结果</span>
   </div>
 </template>
