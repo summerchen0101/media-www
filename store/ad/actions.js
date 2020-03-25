76c5d2800bd6064ecbd0fd352b7a51665b495934
Summer Chen <mp4bj6ruby@gmail.com>
2020-03-25 14:01:22 +0800

    頁面轉跳時不重撈廣告列表

--- store/ad/actions.js
+++ store/ad/actions.js
@@ -1,5 +1,6 @@
 export default {
-  async getAds ({ commit }) {
+  async getAds ({ commit, state }) {
+    if (state.ads.length > 0) { return }
     const res = await this.$api.getAds()
     if (res.code === '0') {
       commit('gotAds', res.data)
