e5a191762a9d06b1b5242f01de1566ee23ce66a5
Summer <mp4bj6ruby@gmail.com>
2020-04-01 09:56:17 +0800

    影片內頁：修改來源判斷bug

--- pages/tv/video-detail.vue
+++ pages/tv/video-detail.vue
@@ -54,13 +54,19 @@ export default {
     Breadcrumb: () => import('@/components/tv/video-detail/Breadcrumb'),
     BlockAd: () => import('@/components/BlockAd')
   },
-  async middleware ({ store, redirect, query }) {
+  async middleware ({ app, store, redirect, query }) {
     await store.dispatch(`${query.category}/getSources`, query.id)
     const sources = store.getters[`${query.category}/sources`]
     const episodeBySource = store.getters[`${query.category}/episodeBySource`]
     if (!(episodeBySource[query.source] && episodeBySource[query.source][query.episode])) {
-      const source = sources[0].id
-      const episode = sources[0].episodes[0].id
+      // 如果query沒有播放集數時, 找尋有播放集數的來源
+      const i = sources.findIndex(t => t.episodes.length > 0)
+      if (i === -1) {
+        app.router.app.$alert('沒有播放來源')
+        return redirect('/')
+      }
+      const source = sources[i].id
+      const episode = sources[i].episodes[0].id
       redirect({ name: 'tv-video-detail', query: { ...query, source, episode, t: new Date().getTime() } })
     }
   },
