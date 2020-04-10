10cb9fd84902ae1be1edd0aca17c51cb8bdfdac9
Summer <mp4bj6ruby@gmail.com>
2020-04-10 15:49:32 +0800

    首頁: 將各影片區塊變成單元組件

---
+++ components/index/CategoryLine.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="index_block">
+    <div class="container">
+      <a id="section4" />
+      <div class="head_title">
+        <span class="index_mtitle">
+          <i :class="toCategoryIcon(category)" />{{ category | enum('category') }}
+        </span>
+
+        <nuxt-link
+          :to="{name: 'tv-filter-category', params: {category}}"
+          class="index_more"
+        >
+          更多
+        </nuxt-link>
+      </div>
+      <!--head_title end-->
+      <div class="index_contentBox">
+        <div class="slider_list">
+          <VideoItem
+            v-for="(video, i) in list"
+            :key="i"
+            :video="video"
+            class="index_slide_unit"
+            category="anime"
+          />
+        </div>
+      </div>
+      <!--index_content end-->
+      <BlockAd :ad="ad" />
+    </div>
+    <!--container end-->
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    BlockAd: () => import('@/components/BlockAd')
+  },
+  props: {
+    category: {
+      type: String,
+      default: ''
+    },
+    ad: {
+      type: Object,
+      default: () => ({})
+    },
+    list: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -23,130 +23,11 @@
       <img src="/images/index/banner-default.png">
     </div>
     <div class="index_container">
-      <div class="index_block">
-        <div class="container">
-          <a id="section1" />
-          <div class="head_title">
-            <span class="index_mtitle">
-              <i class="icon-television" />戏剧
-            </span>
-            <nuxt-link
-              :to="{name: 'tv-filter-category', params: {category: 'drama'}}"
-              class="index_more"
-            >
-              更多
-            </nuxt-link>
-          </div>
-          <!--head_title end-->
-          <div class="index_contentBox index_contentBox_column">
-            <div class="index_tv_list">
-              <VideoItem v-for="(video, i) in dramaList" :key="i" :video="video" category="drama" />
-            </div>
-            <!--index_tv_list end-->
-            <RankBox category="drama" />
-            <!--rank_box end-->
-          </div>
-          <!--index_content end-->
-          <BlockAd :ad="fixedBlockAds[0]" />
-        </div>
-        <!--container end-->
-      </div>
-      <div class="index_block">
-        <div class="container">
-          <a id="section2" />
-          <div class="head_title">
-            <span class="index_mtitle">
-              <i class="icon-television" />电影
-            </span>
-            <nuxt-link
-              :to="{name: 'tv-filter-category', params: {category: 'movie'}}"
-              class="index_more"
-            >
-              更多
-            </nuxt-link>
-          </div>
-          <!--head_title end-->
-          <div class="index_contentBox index_contentBox_column">
-            <div class="index_tv_list">
-              <VideoItem v-for="(video, i) in movieList" :key="i" :video="video" category="movie" />
-            </div>
-            <!--index_tv_list end-->
-            <RankBox category="movie" />
-            <!--rank_box end-->
-          </div>
-          <!--index_content end-->
-          <BlockAd :ad="fixedBlockAds[1]" />
-        </div>
-        <!--container end-->
-      </div>
-      <div class="index_block">
-        <div class="container">
-          <a id="section3" />
-          <div class="head_title">
-            <span class="index_mtitle">
-              <i class="icon-karaoke-microphone-icon" />综艺
-            </span>
-            <nuxt-link
-              :to="{name: 'tv-filter-category', params: {category: 'variety'}}"
-              class="index_more"
-            >
-              更多
-            </nuxt-link>
-          </div>
-          <!--head_title end-->
-          <div class="index_contentBox index_contentBox_column">
-            <div class="index_movie_list">
-              <VideoItem
-                v-for="(video, i) in varietyList"
-                :key="i"
-                :video="video"
-                category="variety"
-              />
-            </div>
-            <!--index_tv_list end-->
-            <RankBox category="variety" />
-          </div>
-          <!--index_content end-->
-          <BlockAd :ad="fixedBlockAds[2]" />
-        </div>
-        <!--container end-->
-      </div>
-      <!--index block end-->
-      <div class="index_block">
-        <div class="container">
-          <a id="section4" />
-          <div class="head_title">
-            <span class="index_mtitle">
-              <i class="icon-cartoons-character-with-big-eyes" />动漫
-            </span>
-
-            <nuxt-link
-              :to="{name: 'tv-filter-category', params: {category: 'anime'}}"
-              class="index_more"
-            >
-              更多
-            </nuxt-link>
-          </div>
-          <!--head_title end-->
-          <div class="index_contentBox">
-            <div class="slider_list">
-              <VideoItem
-                v-for="(video, i) in animeList"
-                :key="i"
-                :video="video"
-                class="index_slide_unit"
-                category="anime"
-              />
-            </div>
-          </div>
-          <!--index_content end-->
-          <BlockAd :ad="fixedBlockAds[3]" />
-        </div>
-        <!--container end-->
-      </div>
-      <!--index block end-->
+      <CategoryBlock category="drama" :ad="fixedBlockAds[0]" :list="dramaList" />
+      <CategoryBlock category="movie" :ad="fixedBlockAds[1]" :list="movieList" />
+      <CategoryBlock category="variety" :ad="fixedBlockAds[2]" :list="varietyList" />
+      <CategoryLine category="anime" :ad="fixedBlockAds[3]" :list="animeList" />
     </div>
-    <!--index_container end-->
     <Footer />
     <IndexScrollTop />
   </div>
@@ -160,7 +41,9 @@ export default {
   components: {
     Slick: () => import('vue-slick'),
     BlockAd: () => import('@/components/BlockAd'),
-    RankBox: () => import('@/components/index/RankBox')
+    RankBox: () => import('@/components/index/RankBox'),
+    CategoryBlock: () => import('@/components/index/CategoryBlock'),
+    CategoryLine: () => import('@/components/index/CategoryLine')
   },
   async fetch ({ app, store, redirect }) {
     const actions = [
@@ -218,10 +101,6 @@ export default {
   },
   head () {
     return {
-      // script: [
-      //   { type: 'text/javascript', src: '/js/script.js', defer: true, body: true }
-      // ],
-
       link: [{ rel: 'stylesheet', href: '/css/index.css' }]
     }
   }
