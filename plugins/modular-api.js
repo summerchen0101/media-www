bd57e39920e768930ee65a85d5e3f515e75a3566
Summer <mp4bj6ruby@gmail.com>
2020-04-24 11:27:08 +0800

    移除modular-api的errHandler

--- plugins/modular-api.js
+++ plugins/modular-api.js
@@ -1,22 +1,13 @@
 import ApiHub from 'modular-api'
 // import axios from 'axios'
-import errMap from '@/lib/err/errMap'
-import statusMap from '@/lib/err/statusMap'
 import apiModules from '@/lib/apis'
 
 export default (ctx, inject) => {
-  const errHandlerConfig = {
-    validCode: ['0'],
-    defaultMsg: '錯誤發生 ({code})',
-    errMap,
-    statusMap
-  }
   if (process.server) {
     ctx.$axios.setHeader('referer', `${process.env.PROTOCOL}://${process.env.SSR_TARGET_BRANCH || ctx.req.headers.host}`)
   }
 
   const ApiHubInstance = ApiHub.bind(ctx.$axios)
-  ApiHubInstance.registerErrHandler(errHandlerConfig)
   ApiHubInstance.onResponse((res) => {
     return res.data
   })
