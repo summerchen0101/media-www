4c55970994f1ca30cd439ed563bec07bfec18011
Summer <mp4bj6ruby@gmail.com>
2020-05-07 15:55:05 +0800

    側邊欄改新版

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="menuBar">
     <div class="nav-content">
+      <!-- WEB版 -->
       <ul class="menu_list">
         <li v-for="(c,i) in cates" :key="i">
           <nuxt-link :to="{name: 'tv-filter-category', params: {category: c.code}}">
@@ -28,7 +29,52 @@
         <li v-else>
           <a href="" @click.prevent="openLoginPopup"><span><i class="icon-user" /></span><span>登入</span></a>
         </li>
-      </ul><a />
+      </ul>
+      <!-- WAP版 -->
+      <div class="user-info">
+        <span id="menu-close" @click="hideMobileMenu">×</span>
+        <div class="img">
+          <img src="/images/ic_usericon.png" alt="">
+        </div>
+        <div v-if="$auth.loggedIn" class="link name">
+          {{ $auth.user.account }}
+        </div>
+        <div v-else class="link">
+          会员
+          <a @click.prevent="openLoginPopup"><span>登入</span></a>
+          /
+          <a @click.prevent="openRegisterPopup"><span>註冊</span></a>
+        </div>
+        <div class="login-link">
+          <a href="" @click.prevent="routerAuthDetector({name: 'user-record'})">
+            <i class="fa fa-clock-o" />观看纪录
+          </a>
+          <a href="" @click.prevent="routerAuthDetector({name: 'user-favorite'})">
+            <i class="fa fa-star" />收藏影片
+          </a>
+        </div>
+      </div>
+      <ul class="menu_list mb-menu-list">
+        <li
+          v-for="(c,i) in cates"
+          :key="i"
+          :class="{active: $route.params.category === c.code}"
+        >
+          <nuxt-link :to="{name: 'tv-filter-category', params: {category: c.code}}">
+            {{ c.label }}
+          </nuxt-link>
+        </li>
+        <li :class="{active: $route.name === 'tv-rank'}">
+          <nuxt-link :to="{name: 'tv-rank'}">
+            排行榜
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link :to="{name: 'other-about'}">
+            关于抖影
+          </nuxt-link>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
