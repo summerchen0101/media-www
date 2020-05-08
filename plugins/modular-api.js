b3ead908bb6ba6e114487024aa4d01eb4905f630
Summer <mp4bj6ruby@gmail.com>
2020-05-08 10:43:00 +0800

    平台切換或已開啟的分頁 登入狀態未更新時的頁面權限判斷

--- plugins/modular-api.js
+++ plugins/modular-api.js
@@ -4,21 +4,6 @@ import ErrMapper from 'error-mapper'
 import apiModules from '@/lib/apis'
 
 export default (ctx, inject) => {
-  ctx.$axios.defaults.validateStatus = (status) => {
-    const isValid = status >= 200 && status < 300
-    if (!isValid) {
-      const errStatusHandlerConfig = {
-        templateKey: 'status',
-        map: require('~/lib/err/status'),
-        defaultMsg: '请求状态错误',
-        handleMsg: (msg, code) => {
-          console.warn(msg, code)
-        }
-      }
-      ErrMapper.register(status, errStatusHandlerConfig)
-    }
-    return isValid
-  }
   if (process.server) {
     ctx.$axios.setHeader('referer', `${process.env.PROTOCOL}://${process.env.SSR_TARGET_BRANCH || ctx.req.headers.host}`)
   }
@@ -42,6 +27,24 @@ export default (ctx, inject) => {
       ErrMapper.register(res, errCodeHandlerConfig)
     }
     return res.data
+  })
+
+  ctx.$axios.onResponseError((err) => {
+    if (err.isAxiosError && err.response.status) {
+      const errStatusHandlerConfig = {
+        templateKey: 'status',
+        map: require('~/lib/err/status'),
+        defaultMsg: '请求状态错误',
+        handleMsg: (msg, code) => {
+          console.warn(msg, code)
+          ctx.app.router.app.$alert(msg)
+          if (code === 401) {
+            ctx.redirect('/')
+          }
+        }
+      }
+      ErrMapper.register(err.response.status, errStatusHandlerConfig)
+    }
   })
 
   ApiHubInstance.registerMultiModule(apiModules)
